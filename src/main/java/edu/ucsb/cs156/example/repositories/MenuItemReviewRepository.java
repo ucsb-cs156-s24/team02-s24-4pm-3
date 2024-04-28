776bfc1a6eb1a5e202d3ce21387f790b0c28b22d
MeDotE <mikejiang@ucsb.edu>
2024-04-28 04:58:57 -0700

    mj - finished @repository class and data migration file

--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -1,12 +1,10 @@
 package edu.ucsb.cs156.example.repositories;
 
 import edu.ucsb.cs156.example.entities.MenuItemReview;
-import edu.ucsb.cs156.example.entities.UCSBDate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuItemReviewRepository extends CrudRepository<MenuItemReview, Long> {
     Iterable<MenuItemReview> findAllByItemId(long itemId);
-    Iterable<MenuItemReview> findAllByReviewerEmail(String reviewerEmail);
 }
