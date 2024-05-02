1515572daff238c2288da17cf99513409d570e59
Kevin Lavelle <kevinglavelle@gmail.com>
2024-05-01 17:33:30 -0700

    controller

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -6,7 +6,8 @@ import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String> {
- 
+
 }
